d730d8f51e769f3d58783cf84d5202362bbc57b7
basitdurrani <basitdurrani05786@gmail.com>
2025-03-20 19:49:08 +0530

    Fourth Commit

---
+++ Object-Oriented Programming/constructor_1.java
@@ -0,0 +1,42 @@
+class arbaaz{
+
+
+    int a; String b;
+
+
+    arbaaz(){
+
+        a=30;
+        b="Arbaaz";
+
+    }
+
+    void show(){
+
+        System.out.println(a);
+        System.out.println(b); 
+
+    }
+
+
+
+
+}
+
+
+
+
+public class constructor_1 {
+
+    public static void main(String[] args) {
+
+
+        arbaaz obj = new arbaaz();
+        obj.show();
+
+
+        
+
+    }
+    
+}
